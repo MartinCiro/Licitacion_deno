bad18688efbf8f28c260e2d2baacce9e78c44a52
martinciro <martinciro11@gmail.com>
2023-08-30 20:19:13 -0500

    se anexa loggin

--- src/modules/clientes/routes/clientes.routes.js
+++ src/modules/clientes/routes/clientes.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+//const { isAuthenticatedMW, checkPermissions } = require('../../auth/api/auth.api');
 
 // api handlers
 const { validarUserApi, crearUserApi, delUserApi, updateUserApi, obtenerProductosApi} = require('../api/clientes.api');
@@ -19,6 +20,7 @@ router.post('/validate', validarUserApi);
 router.delete('/deleteUser', delUserApi);
 router.post('/updateUser', updateUserApi);
 router.get('/productos', obtenerProductosApi);
+//router.get('/clientes/leerRut', isAuthenticatedMW, checkPermissions([1, 2]), extractRutAPI); //ejm: uso
 module.exports = router;
 
 
